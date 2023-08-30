0d720717a17c001d0bec7c843007e4679dbd89e5
Matthew Monterosso <MatthewAMonterosso@gmail.com>
2023-08-29 23:45:06 -0500

    11 and partial 12

---
+++ ID_12.py
@@ -0,0 +1,14 @@
+def triangle_number(stopval = 1000):
+    dict = {}
+    for t in range(0,stopval)
+        dict[t] = sum(range(1,t+1))
+    return dict
+
+# in order to have 501 divisors - it has to be larger than 501 - likely much larger...
+
+def check_divisors(dict):
+    divisor_dict = {}
+    for entry in dict.keys()
+
+
+# 6278: 19709781
